7c78225533c32b61caed9fc210066f44dba22095
Oséias Barreto <Oseiasbarreto94@gmail.com>
2026-01-05 15:08:31 -0300

    05/01/2025

---
+++ src/main/java/com/oseias/produtosapi/ProdutosApiApplication.java
@@ -0,0 +1,24 @@
+package com.oseias.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ProdutosApiApplication {
+
+    @GetMapping("/inicial")
+    public String saudar(){
+        return "Hello World";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/oseias/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oseias.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
